50961af2d24c63396190669350ebdf240309a541
Caio Nascimento <caionsantos2000@gmail.com>
2022-04-09 22:56:26 -0300

    chore: styled components and mui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,12 @@
+import { Typography } from "@mui/material";
 import "./App.css";
 
 function App() {
-  return <div className='App'>OLÁ</div>;
+  return (
+    <div className='App'>
+      <Typography>Olá</Typography>
+    </div>
+  );
 }
 
 export default App;
